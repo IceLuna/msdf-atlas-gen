e634751e5f0c9277eb0d3b42280457a64e5d9835
SShikhali <sshikhaliev@luxoft.com>
2023-04-15 20:47:06 +0200

    FreeType + TinyXML + Eagle stuff

---
+++ premake5.lua
@@ -0,0 +1,47 @@
+project "MSDF-Atlas"
+    kind "Staticlib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "Off"
+
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "**.h",
+        "**.cpp"
+    }
+
+    includedirs
+	{
+		"msdf-atlas-gen/",
+		"msdfgen/",
+		"artery-font-format/",
+		"tinyxml2/",
+		"freetype/include/"
+	}
+
+    defines
+    {
+        "MSDF_ATLAS_PUBLIC="
+    }
+
+    links
+    {
+        "freetype.lib"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter { "system:windows", "configurations:Debug" }
+        runtime "Debug"
+
+    filter { "system:windows", "configurations:Release" }
+        runtime "Release"
+        optimize "On"
+
+    filter { "system:windows", "configurations:Dist" }
+        runtime "Release"
+        optimize "On"
